ccbbec12d09cf7ac986f4d76626f043e05e5a3f7
Alejandro Garcia <alejandro.garciator@gmail.com>
2021-05-17 10:38:07 +0200

    aaa

--- leerFicheroXML.js
+++ leerFicheroXML.js
@@ -1,14 +1,16 @@
 function gestionarFicheroXML(xmlDoc){
-	let capaVacia = document.querySelector("#ficheroXML")
+/*	let capaVacia = document.querySelector("#ficheroXML")
 	let alumnos = xmlDoc.querySelectorAll("alumno")
 	for(let i=0; i<alumnos.length; i++)
 		capaVacia.innerHTML = capaVacia.innerHTML + "<p>" + alumnos[i].textContent + "</p>"
-	
+	*/
+	alert()
 }
 
-let capa = document.querySelector("div:nth-child(1)") 
+/*let capa = document.querySelector("div:nth-child(1)") 
 capa.addEventListener("click",CargarFichero);
 function CargarFichero()
 {
 	loadDocA("leerFicheroXML.xml","xml");
 }
+*/
